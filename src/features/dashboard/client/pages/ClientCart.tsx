9d2d8aa48191e54d992c6d2018a8df220d288cae
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-18 16:26:27 +1000

    upd: client -order and -cart pages

--- src/features/dashboard/client/pages/ClientCart.tsx
+++ src/features/dashboard/client/pages/ClientCart.tsx
@@ -1,5 +1,6 @@
 import { DashboardLayout } from '@/features/dashboard/DashboardLayout.tsx';
 import { useRoleMenu } from '@/shared/hooks/useRoleMenu.tsx';
+import { Card } from '@/shared/ui/card.tsx';
 
 export const ClientCart = () => {
   const menu = useRoleMenu();
@@ -7,6 +8,9 @@ export const ClientCart = () => {
   return (
     <DashboardLayout roleBasedMenuSlot={menu}>
       <h1 className="text-2xl font-bold mb-8">Моя корзина</h1>
+      <Card className="p-4 w-full min-h-1/3 mx-auto justify-center">
+        <h3 className="text-center">Корзина пока пуста :(</h3>
+      </Card>
     </DashboardLayout>
   );
 };
--- src/features/dashboard/client/pages/ClientOrder.tsx
+++ src/features/dashboard/client/pages/ClientOrder.tsx
@@ -1,5 +1,6 @@
 import { DashboardLayout } from '@/features/dashboard/DashboardLayout.tsx';
 import { useRoleMenu } from '@/shared/hooks/useRoleMenu.tsx';
+import { Card } from '@/shared/ui/card.tsx';
 
 export const ClientOrder = () => {
   const menu = useRoleMenu();
@@ -7,6 +8,9 @@ export const ClientOrder = () => {
   return (
     <DashboardLayout roleBasedMenuSlot={menu}>
       <h1 className="text-2xl font-bold mb-8">Мои заказы</h1>
+      <Card className="p-4 w-full min-h-1/3 mx-auto justify-center">
+        <h3 className="text-center">Пока заказов нет :(</h3>
+      </Card>
     </DashboardLayout>
   );
 };
