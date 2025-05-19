7f78a153f5e5f00d2cb2609e7bf368afcfc4354c
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-19 23:18:02 +1000

    feat: add api for admin (draft)

---
+++ src/shared/api/admin.ts
@@ -0,0 +1,19 @@
+import api from './http';
+
+export const banUserByLogin = (login: string) =>
+  api.post('admin/ban', login);
+
+export const unBanUserByLogin = (login: string) =>
+  api.post('admin/unBan', login);
+
+export const getAllSuppliers = () =>
+  api.get('admin/suppliers');
+
+export const getAllClients = () =>
+  api.get('admin/clients');
+
+export const getAllProducts = () =>
+  api.get('admin/products');
+
+export const removeProductById = (id: number) =>
+  api.del(`admin/products/delete/${id}`);
