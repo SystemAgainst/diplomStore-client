614ff3b122c6b66bbcc8adc1e0eaef048d19cb32
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-21 09:06:15 +1000

    code brush

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -8,7 +8,7 @@ export type Role = (typeof ROLES)[keyof typeof ROLES];
 
 export const API_URL = import.meta.env.VITE_API_URL;
 
-export const getFullImageUrl = (path: string) => {
+export const getFullImageUrl = (path: string | undefined) => {
   if (!path) return '/mock-product.jpg';
   return path.startsWith('http')
     ? path
