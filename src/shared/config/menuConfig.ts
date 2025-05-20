ce076b728292779092f5aead49c2e866fc760133
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-20 17:08:41 +1000

    upd: rename menu item

--- src/shared/config/menuConfig.ts
+++ src/shared/config/menuConfig.ts
@@ -1,16 +1,16 @@
 export const menuConfig = {
   "SUPPLIER": [
-    { label: 'Главная', path: '/SUPPLIER/me' },
+    // { label: 'Главная', path: '/SUPPLIER/me' },
     { label: 'Мои продукты', path: '/SUPPLIER/products' },
-    { label: 'Мои заказы', path: '/SUPPLIER/orders' },
+    { label: 'Заявка на покупку', path: '/SUPPLIER/orders' },
   ],
   "SOLE_TRADER": [
     { label: 'Главная', path: 'SOLE_TRADER/me' },
-    { label: 'Статус заказа', path: '/SOLE_TRADER/order' },
     { label: 'Моя корзина', path: '/SOLE_TRADER/cart' },
+    { label: 'Статус заказа', path: '/SOLE_TRADER/order' },
   ],
   "ADMIN": [
-    { label: 'Главная', path: '/ADMIN/me' },
+    // { label: 'Главная', path: '/ADMIN/me' },
     { label: 'Клиенты', path: '/ADMIN/clients' },
     { label: 'Поставщики', path: '/ADMIN/suppliers' },
     { label: 'Все товары', path: '/ADMIN/products' },
