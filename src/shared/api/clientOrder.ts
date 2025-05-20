7f3599d54f12977a8d17bb625973a8d13795c923
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-20 17:30:23 +1000

    upd: replace api

--- src/shared/api/clientOrder.ts
+++ src/shared/api/clientOrder.ts
@@ -5,7 +5,8 @@ import type { ClientLocation } from '@/shared/api/dto/client.ts';
 export const createOrder = (data: ClientLocation) =>
   api.postWithoutBody('order/create', data);
 
-export const getAllClientOrders = () => api.get(`client/my/orders`);
+export const getAllClientOrders = () =>
+  api.get(`client/my/orders`);
 
 // Проверка корзины перед оформлением (актуальность)
 export const checkoutOrder = () =>
@@ -14,15 +15,3 @@ export const checkoutOrder = () =>
 // Оплатить заказ по ID
 export const payOrder = (id: number) =>
   api.postWithoutBody(`order/pay/${id}`);
-
-export const orderShipped = (id: number) =>
-  api.postWithoutBody(`order/shipped/${id}`);
-
-export const orderDelivered = (id: number) =>
-  api.postWithoutBody(`order/delivered/${id}`);
-
-export const orderCanceled = (id: number) =>
-  api.postWithoutBody(`order/cancelled/${id}`);
-
-export const orderConfirmed = (id: number) =>
-  api.postWithoutBody(`order/confirmed/${id}`);
--- src/shared/api/supplier.ts
+++ src/shared/api/supplier.ts
@@ -1,3 +1,16 @@
 import api from './http';
 
-export const getSupplierAllProducts = () => api.get('supplier/my/products');
+export const getSupplierAllProducts = () =>
+  api.get('supplier/my/products');
+
+export const orderShipped = (id: number) =>
+  api.postWithoutBody(`order/shipped/${id}`);
+
+export const orderDelivered = (id: number) =>
+  api.postWithoutBody(`order/delivered/${id}`);
+
+export const orderCanceled = (id: number) =>
+  api.postWithoutBody(`order/cancelled/${id}`);
+
+export const orderConfirmed = (id: number) =>
+  api.postWithoutBody(`order/confirmed/${id}`);
