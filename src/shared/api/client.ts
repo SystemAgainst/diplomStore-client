99eedd1bfdad3b65dece467f7acf2478aca282a3
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-05-25 12:44:47 +1000

    feat: new api for detail order for client

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -2,3 +2,6 @@ import api from './http';
 
 export const getAllInfoAboutSupplier = (login: string) =>
   api.get(`user/${login}`);
+
+export const getDetailInfoAboutOrder = (id: number) =>
+  api.get(`client/my/orders/${id}`);
